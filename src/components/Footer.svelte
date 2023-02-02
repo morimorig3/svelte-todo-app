2642854bdbfe8543dc6980d9099af6995de5f053
morimorig3 <mori.nto@outlook.jp>
2023-02-02 09:00:17 +0900

    feat:add Header Footer Component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,19 @@
+<script>
+	let year = new Date().getFullYear();
+</script>
+
+<div>
+	<small>©{year} morimorig3</small>
+</div>
+
+<style>
+	div {
+		height: 24px;
+		background-color: #333333;
+		display: grid;
+		place-items: center;
+	}
+	small {
+		font-weight: bold;
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<h1>Todo List by Svelte</h1>
+
+<style>
+	h1 {
+		text-align: center;
+		padding: 1em 0;
+	}
+</style>
