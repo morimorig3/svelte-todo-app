96e1a4e67d5b69322f1473f0c0829c162d3d4154
morimorig3 <mori.nto@outlook.jp>
2023-02-05 10:32:23 +0900

    style:label style adjust

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -12,19 +12,17 @@
 </li>
 
 <style>
-	li {
+	label {
+		display: flex;
+		gap: 8px;
 		margin-bottom: 16px;
 		padding: 16px;
 		border: 1px solid rgba(149, 157, 165, 0.2);
 		transition: 0.25s;
 	}
-	li:hover {
+	label:hover {
 		border-color: transparent;
 		box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-	}
-	label {
-		display: flex;
-		gap: 8px;
 	}
 	p {
 		font-weight: bold;
