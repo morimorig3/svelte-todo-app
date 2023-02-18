21a5ed7adc1e9361ebbf367b2538c3ad44c3073c
morimorig3 <mori.nto@outlook.jp>
2023-02-18 13:24:51 +0900

    feat:close button style

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import { slide } from 'svelte/transition';
+	import { fade, slide } from 'svelte/transition';
 
 	import { todos } from '../store';
+	import IconTrashBox from './IconTrashBox.svelte';
+	import TodoOptions from './TodoOptions.svelte';
 	export let id: string;
 	export let title: string;
 	export let isCompleted: boolean;
@@ -35,17 +37,24 @@
 				<p class="todo__title">{title}</p>
 			{/if}
 		</div>
-		<div class="todo__options">
-			<button class="edit button" on:click={() => toggleIsEdit(id)} />
-			{#if isCompleted}
-				<button class="close button" on:click={() => removeTodo(id)}>×</button>
-			{/if}
+		<div class="todo__buttons">
+			<button class="button" on:click={() => toggleIsEdit(id)}>
+				<TodoOptions />
+			</button>
 		</div>
+		{#if isCompleted}
+			<button
+				transition:fade={{ duration: 100 }}
+				class="close-button"
+				on:click={() => removeTodo(id)}
+			/>
+		{/if}
 	</label>
 </li>
 
 <style>
 	.todo {
+		position: relative;
 		display: flex;
 		justify-content: space-between;
 		align-items: stretch;
@@ -73,35 +82,33 @@
 		word-break: break-all;
 	}
 	.button {
+		padding: 4px;
 		flex-shrink: 0;
 		width: 25px;
 		height: 25px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		line-height: 1;
 		border: none;
-		font-weight: bold;
-		border-radius: 10px;
-	}
-	.button.close {
-		background-color: red;
-		color: #ffffff;
-	}
-	.edit {
-		border: none;
+		border-radius: 3px;
 		background-color: transparent;
-		width: 20px;
-		height: 20px;
-		background-size: contain;
-		background-repeat: no-repeat;
-		background-image: url("data:image/svg+xml;charset=utf8,%3Csvg%20width%3D'24'%20height%3D'24'%20viewBox%3D'0%200%2024%2024'%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20xmlns%3Axlink%3D'http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink'%3E%3Crect%20width%3D'24'%20height%3D'24'%20stroke%3D'none'%20fill%3D'%23000000'%20opacity%3D'0'%2F%3E%3Cg%20transform%3D%22matrix(1.05%200%200%201.05%2012%2012)%22%20%3E%3Cpath%20style%3D%22stroke%3A%20none%3B%20stroke-width%3A%201%3B%20stroke-dasharray%3A%20none%3B%20stroke-linecap%3A%20butt%3B%20stroke-dashoffset%3A%200%3B%20stroke-linejoin%3A%20miter%3B%20stroke-miterlimit%3A%204%3B%20fill%3A%20rgb(0%2C0%2C0)%3B%20fill-rule%3A%20nonzero%3B%20opacity%3A%201%3B%22%20transform%3D%22%20translate(-12.5%2C%20-11.5)%22%20d%3D%22M%2018.414062%202%20C%2018.158188%202%2017.902031%202.0974687%2017.707031%202.2929688%20L%2016%204%20L%2020%208%20L%2021.707031%206.2929688%20C%2022.098031%205.9019687%2022.098031%205.2689063%2021.707031%204.8789062%20L%2019.121094%202.2929688%20C%2018.925594%202.0974687%2018.669937%202%2018.414062%202%20z%20M%2014.5%205.5%20L%203%2017%20L%203%2021%20L%207%2021%20L%2018.5%209.5%20L%2014.5%205.5%20z%22%20stroke-linecap%3D%22round%22%20%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E");
+	}
+	.close-button {
+		position: absolute;
+		top: -10px;
+		right: -10px;
+		border: none;
+		border-radius: 100vh;
+		width: 25px;
+		height: 25px;
+		background-color: transparent;
+		background-image: url("data:image/svg+xml;charset=utf8,%3Csvg%20clip-rule%3D%22evenodd%22%20fill-rule%3D%22evenodd%22%20stroke-linejoin%3D%22round%22%20stroke-miterlimit%3D%222%22%20viewBox%3D%220%200%2024%2024%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20fill%3D'%23ef4444'%20d%3D%22m12.002%202.005c5.518%200%209.998%204.48%209.998%209.997%200%205.518-4.48%209.998-9.998%209.998-5.517%200-9.997-4.48-9.997-9.998%200-5.517%204.48-9.997%209.997-9.997zm0%208.933-2.721-2.722c-.146-.146-.339-.219-.531-.219-.404%200-.75.324-.75.749%200%20.193.073.384.219.531l2.722%202.722-2.728%202.728c-.147.147-.22.34-.22.531%200%20.427.35.75.751.75.192%200%20.384-.073.53-.219l2.728-2.728%202.729%202.728c.146.146.338.219.53.219.401%200%20.75-.323.75-.75%200-.191-.073-.384-.22-.531l-2.727-2.728%202.717-2.717c.146-.147.219-.338.219-.531%200-.425-.346-.75-.75-.75-.192%200-.385.073-.531.22z%22%20fill-rule%3D%22nonzero%22%2F%3E%3C%2Fsvg%3E");
 	}
 	input[type='text'] {
 		color-scheme: light;
 	}
-	.todo__options {
+	.todo__buttons {
 		display: flex;
 		column-gap: 8px;
+	}
+	.button {
+		background-color: transparent;
 	}
 </style>
