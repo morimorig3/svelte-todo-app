e30229bf129e961a8c57d2956aa40a604506baa6
morimorig3 <mori.nto@outlook.jp>
2023-02-18 11:44:50 +0900

    feat:add initial todo param const

--- src/param/const.ts
+++ src/param/const.ts
@@ -1,5 +1,16 @@
+import type { TodoType } from '../types';
+
 export const LOCAL_STORAGE_KEY = 'svelte-todo-app';
+
+export const SAMPLE_TASK = [];
 
 export const VALIDATION_MESSAGE_BLANK_TEXT = 'Please enter a task title...';
 
 export const ADD_TODO_TEXT_BOX_PLACEHOLDER = 'Enter a task';
+
+export const INITIAL_TODO_PARAM: TodoType = {
+	id: '',
+	title: '',
+	isCompleted: false,
+	isEdit: false
+};
